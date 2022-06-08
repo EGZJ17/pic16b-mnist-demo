c25bab282ad5e1f2ae01a52e3f3fbb4f26ed51bd
EGZJ17 <55767984+EGZJ17@users.noreply.github.com>
2022-06-07 18:14:15 -0700

    reduce inputs

--- app/__init__.py
+++ app/__init__.py
@@ -45,11 +45,6 @@ def resume():
     school1GPA=request.form['school1GPA']
     school1Relevant_Courses=request.form['school1Relevant_Courses']
 
-    school2=request.form['school2']
-    school2startdate=request.form['school2startdate']
-    school2enddate=request.form['school2enddate']
-    school2GPA=request.form['school2GPA']
-    school2Relevant_Courses=request.form['school2Relevant_Courses']
 
     company1=request.form['company1']
     company1position=request.form['company1position']
@@ -67,27 +62,19 @@ def resume():
     company2experience2=request.form['company2experience2']
     company2experience3=request.form['company2experience3']
 
-    company3=request.form['company3']
-    company3position=request.form['company3position']
-    company3startdate=request.form['company3startdate']
-    company3enddate=request.form['company3enddate']
-    company3experience1=request.form['company3experience1']
-    company3experience2=request.form['company3experience2']
-    company3experience3=request.form['company3experience3']
-
     Achievement1=request.form['Achievement1']
     Achievement1description1=request.form['Achievement1description1']
-    Achievement1description2=request.form['Achievement1description2']
-    
-    Achievement2=request.form['Achievement2']
-    Achievement2description1=request.form['Achievement2description1']
-    Achievement2description2=request.form['Achievement2description2']
-    
+
     languages=request.form['languages']
     technical_skills=request.form['technical_skills']
     interests=request.form['interests']
 
-    return render_template('resume.html', first_name=first_name, last_name=last_name, location=location, phone=phone, email=email, school1=school1, school1startdate=school1startdate, school1enddate=school1enddate, school1GPA=school1GPA, school1Relevant_Courses=school1Relevant_Courses, school2=school2, school2startdate=school2startdate, school2enddate=school2enddate, school2GPA=school2GPA, school2Relevant_Courses=school2Relevant_Courses, company1=company1, company1position=company1position, company1startdate=company1startdate, company1enddate=company1enddate, company1experience1=company1experience1, company1experience2=company1experience2, company1experience3=company1experience3, company2=company2, company2position=company2position, company2startdate=company2startdate, company2enddate=company2enddate, company2experience1=company2experience1, company2experience2=company2experience2, company2experience3=company2experience3, company3=company3, company3position=company3position, company3startdate=company3startdate, company3enddate=company3enddate, company3experience1=company3experience1, company3experience2=company3experience2, company3experience3=company3experience3, Achievement1=Achievement1, Achievement1description1=Achievement1description1, Achievement1description2=Achievement1description2, Achievement2=Achievement2, Achievement2description1=Achievement2description1, Achievement2description2=Achievement2description2, languages=languages, technical_skills=technical_skills, interests=interests)
+    return render_template('resume.html', first_name=first_name, last_name=last_name, location=location, phone=phone, email=email, 
+    school1=school1, school1startdate=school1startdate, school1enddate=school1enddate, school1GPA=school1GPA, school1Relevant_Courses=school1Relevant_Courses, 
+    company1=company1, company1position=company1position, company1startdate=company1startdate, company1enddate=company1enddate, company1experience1=company1experience1, company1experience2=company1experience2, company1experience3=company1experience3, 
+    company2=company2, company2position=company2position, company2startdate=company2startdate, company2enddate=company2enddate, company2experience1=company2experience1, company2experience2=company2experience2, company2experience3=company2experience3, 
+    Achievement1=Achievement1, Achievement1description1=Achievement1description1, 
+    languages=languages, technical_skills=technical_skills, interests=interests)
     #abort(404)
 
 
