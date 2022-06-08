e87d163b5ddeeedf30f45bdc4efcd8a3c6ea18ba
dhuang135 <dhuang135@ucla.edu>
2022-06-07 18:21:13 -0700

    yay

--- app/__init__.py
+++ app/__init__.py
@@ -85,14 +85,6 @@ def submit():
         return render_template('indeed_test.html')
     else:
         try:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-            driver.get("https://medium.com")
-            source = driver.page_source
             return render_template('indeed_test.html', source=source)
         except:
             return render_template('indeed_test.html', error=True)
